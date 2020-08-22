063fc1f62afe479cf4ff38a9d3071795511a8db7
darshnablah <dwright758@gmail.com>
2020-08-21 22:33:17 -0500

    added user functionality

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-//const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-//const session = require('express-session');
+const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess= {
+  secret: 'Super duper secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
