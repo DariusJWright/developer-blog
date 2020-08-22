eacea8b59e49bdf27dfda38cf35788b9b63679bd
darshnablah <dwright758@gmail.com>
2020-08-21 21:06:04 -0500

    first commit albeit a bit late

---
+++ models/User.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(pw) {
+    return bcrypt.compareSync(pw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    }
+  },
+  {
+    hooks: {
+      async beforeCreate(newUser) {
+        newUser.password = await bcrypt.hash(newUser.password, 15);
+        return newUser;
+      },
+      async beforeUpdate(updateUser) {
+        updateUser.password = await bcrypt.hash(updateUser.password, 15);
+        return updateUser;
+      }
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+//const routes = require('./controllers');
+const sequelize = require('./config/connection');
+//const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}`);
+    console.log('=======================================================');
+    console.log('=======================================================');
+    console.log('=======================================================');
+  });
+});
